f4d52155d51c00925a07830acadd6270745006bc
yavuzyarkinokular <yarkinokular@gmail.com>
2022-12-23 23:52:05 +0300

    all js files working

--- intro/src/App.js
+++ intro/src/App.js
@@ -1,34 +1,52 @@
 import Navi from "./Navi";
 import CategoryList from "./CategoryList";
 import ProductList from "./ProductList";
+//reactstrap ekleme
 import { Container, Row, Col } from "reactstrap";
-function App() {
-  let productInfo = {
-    title: "Product List",
-    subtitle: "Bünyemizde ki araç kategorileri aşağıdaki gibidir   ",
-  };
-  let categoryInfo = {
-    title: "Category List",
-    subtitle: "Bünyemizdeki araçların listesi",
-  };
-  return (
-    <div>
-      <Container>
-        <Row>
-          <Navi></Navi>
-        </Row>
+import "bootstrap/dist/css/bootstrap.min.css";
+//reactstrap bitiş
+import React, { Component } from "react";
 
-        <Row>
-          <Col xs="6">
-            <CategoryList info={productInfo}></CategoryList>
-          </Col>
-          <Col xs="6">
-            <ProductList info={categoryInfo}></ProductList>
-          </Col>
-        </Row>
-      </Container>
-    </div>
-  );
+export default class App extends Component {
+  state = {
+    currentCategory: "",
+  };
+  changeCategory = (category) => {
+    this.setState({ currentCategory: category.type });
+  };
+  render() {
+    let productInfo = {
+      title: "Product List",
+      subtitle: "Bünyemizde ki araç kategorileri aşağıdaki gibidir   ",
+    };
+    let categoryInfo = {
+      title: "Category List",
+      subtitle: "Bünyemizdeki araçların listesi",
+    };
+    return (
+      <div>
+        <Container>
+          <Row>
+            <Navi></Navi>
+          </Row>
+
+          <Row>
+            <Col xs="6">
+              <CategoryList
+                currentCategory={this.state.currentCategory}
+                changeCategory={this.changeCategory}
+                info={categoryInfo}
+              ></CategoryList>
+            </Col>
+            <Col xs="6">
+              <ProductList
+                currentCategory={this.state.currentCategory}
+                info={productInfo}
+              ></ProductList>
+            </Col>
+          </Row>
+        </Container>
+      </div>
+    );
+  }
 }
-
-export default App;
--- intro/src/CategoryList.js
+++ intro/src/CategoryList.js
@@ -1,33 +1,33 @@
 import React, { Component } from "react";
 import { ListGroup, ListGroupItem } from "reactstrap";
 
-class CategoryList extends Component {
-  //State Yapısı ile Tutulan Değerin Çekilmesi
-  constructor(props) {
-    super(props);
-    this.state = {
-      aracsegmentleri: [
-        { id: 1, aracsegmenti: "Manuel Benzin" },
-        { id: 2, aracsegmenti: "Manuel Dizel" },
-        { id: 3, aracsegmenti: "Otomatik Benzin" },
-        { id: 4, aracsegmenti: "Otomatik Dizel" },
-        { id: 5, aracsegmenti: "Ticari" },
-      ],
-    };
-  }
+export default class CategoryList extends Component {
+  state = {
+    aracsegmentleri: [
+      { id: 1, type: "Manuel Benzin" },
+      { id: 2, type: "Manuel Dizel" },
+      { id: 3, type: "Otomatik Benzin" },
+      { id: 4, type: "Otomatik Dizel" },
+      { id: 5, type: "Ticari" },
+    ],
+  };
+
   render() {
     return (
       <div>
         <ListGroup>
           <h1> {this.props.info.title}</h1>
           <h3> {this.props.info.subtitle}</h3>
-          <h3> Müsaitlik Durumu: {this.state.sayac}</h3>
-          {this.state.aracsegmentleri.map((aracsegmentleri) => (
-            <ListGroupItem active>{aracsegmentleri.aracsegmenti}</ListGroupItem>
+          {this.state.aracsegmentleri.map((category) => (
+            <ListGroupItem
+              onClick={() => this.props.changeCategory(category)}
+              key={category.id}
+            >
+              {category.type}
+            </ListGroupItem>
           ))}
         </ListGroup>
       </div>
     );
   }
 }
-export default CategoryList;
